696d2e43e96d42b4fd3c0690ed64d6cbf813dfe0
seokinson <46913648+seokinson@users.noreply.github.com>
2019-04-30 22:36:09 +0900

    Update and rename auto_pilot_seokin.py to auto_pilot.py

--- auto_pilot.py
+++ auto_pilot.py
@@ -25,67 +25,50 @@ if __name__=='__main__':
     astar = Astar(json_file="./utils/airsim_nh.json")
 
     done = False
-    coordinates_degree = 0
+    coordinates_degree_err = 0
     d=0
     while True:
         start_point, start_direction, coordinates = astar.compute(path_continue=done)
-        #a = len(coordinates)
-        #coordinates_sparse = [ coordinates[i] for i in range(a) if i % 3 ==0]
-
+        
         if done == False:
             # 최초 상태일때 차량 이동 그 이후에는 직전 end_point에서 바로 시작
 
             car_controller.setPosition(start_point, start_direction)
-            time.sleep(3)
-        
-        
-        
+            time.sleep(3)        
         
         #pid
         while True :
 
-            
-
         
-
-            
-
-            
-
-            if coordinates_degree %90 ==0 :    
+            if coordinates_degree_err %90 ==0 :    
 
                
 
                 time.sleep(0.05)
 
-                _, d, coordinates_degree,err_degree = car_controller.compute_1(coordinates)
-
-
-        
-
-    
-
-             
-
-            # print("Center Distance", center_distance)
-
+                coordinates_degree_err = car_controller.compute_straight(coordinates)
             
 
-            else :
+            
+            elif coordinates_degree_err < 0 :
 
-                for i in range (100) :
+                for i in range (130) :
 
                     time.sleep(0.05)
 
-                    _, d, coordinates_degree,err_degree = car_controller.compute_2(coordinates)
-
+                    coordinates_degree_err = car_controller.compute_left(coordinates)
                 
+            else :
 
+                for i in range (130) :
+                        
+                    time.sleep(0.05)
 
+                    coordinates_degree_err = car_controller.compute_right(coordinates)
                     
-
-
         car_controller.coord_index = 1
 
 
         
+
+
