9ad57396bb8d1640613e768f61bf854254a4e7c9
genie0204 <48711578+genie0204@users.noreply.github.com>
2019-05-13 12:07:01 +0900

    Update auto_pilot.py

--- auto_pilot.py
+++ auto_pilot.py
@@ -22,6 +22,10 @@ class AutoPilot():
         self.client2 = airsim.CarClient(ip)
         self.client2.confirmConnection()
 
+        self.client_image = airsim.CarClient(ip)
+        self.client_image.confirmConnection()
+
+
         self.car_controller = CarController(self.client, "Car1")
         self.car_controller2 = CarController(self.client2, "Car2")
 
@@ -55,7 +59,104 @@ class AutoPilot():
 
         return math.sqrt((x1-x2)**2+(y1-y2)**2)
 
-    def drive(self):
+    def getImage(self,car_name):
+
+        if car_name == "Car1" :
+            responses = self.client_image.simGetImages([
+            airsim.ImageRequest("MyCam1", airsim.ImageType.Scene),  
+            airsim.ImageRequest("MyCam2", airsim.ImageType.Scene), 
+            airsim.ImageRequest("MyCam3", airsim.ImageType.Scene), 
+            airsim.ImageRequest("MyCam4", airsim.ImageType.Scene)])
+
+        if car_name == "Car2" :
+
+            responses = self.client_image.simGetImages([
+    
+            airsim.ImageRequest("MyCam5", airsim.ImageType.Scene),
+            airsim.ImageRequest("MyCam6", airsim.ImageType.Scene),
+            airsim.ImageRequest("MyCam7", airsim.ImageType.Scene),
+            airsim.ImageRequest("MyCam8", airsim.ImageType.Scene)]) 
+            
+        return responses
+
+    def getCollisioninfo(self,car_name):
+
+        if car_name=="Car1":
+            collision_info = client.simGetCollisionInfo()
+            if collision_info.has_collided:
+                print("Collision at pos %s, normal %s, impact pt %s, penetration %f, name %s, obj id %d" % (
+                    pprint.pformat(collision_info.position), 
+                    pprint.pformat(collision_info.normal), 
+                    pprint.pformat(collision_info.impact_point), 
+                    collision_info.penetration_depth, collision_info.object_name, collision_info.object_id))
+                break
+            time.sleep(0.1)
+
+        if car_name=="Car2":
+            collision_info = client.simGetCollisionInfo()
+            if collision_info.has_collided:
+                print("Collision at pos %s, normal %s, impact pt %s, penetration %f, name %s, obj id %d" % (
+                    pprint.pformat(collision_info.position), 
+                    pprint.pformat(collision_info.normal), 
+                    pprint.pformat(collision_info.impact_point), 
+                    collision_info.penetration_depth, collision_info.object_name, collision_info.object_id))
+                break
+            time.sleep(0.1)    
+
+        return collision_info
+    
+
+    def getSlope(self, p1, p2):
+        '''Get the slope of a line segment'''
+        (x1, y1), (x2, y2) = p1, p2
+        try:
+            return (float(y2)-y1)/(float(x2)-x1)
+        except ZeroDivisionError:
+            # line is vertical
+            return None
+
+    def getYintercept(self, slope, p1):
+        '''Get the y intercept of a line segment'''
+        if slope != None:
+            x, y = p1
+            return y - slope * x
+        else:
+            return None
+
+
+
+    def getDistanceFromCenterLine(self,car_name):
+                    
+        if car_name=="Car1":
+            pos_x = car_state.kinematics_estimated.position.x_val
+            pos_y = car_state.kinematics_estimated.position.y_val
+            self.storePos(pos_x, pos_y)
+            target = airsim.Vector3r(coordinates[self.coord_index][0],coordinates[self.coord_index][1],0)
+            target_x = target.x_val
+            target_y = target.y_val
+            
+            return math.sqrt((pos_x-target_x)**2+(pos_y-target_y)**2)
+
+     
+        if car_name=="Car2":
+
+            pos_x = car_state.kinematics_estimated.position.x_val
+            pos_y = car_state.kinematics_estimated.position.y_val
+            self.storePos(pos_x, pos_y)
+            target = airsim.Vector3r(coordinates[self.coord_index][0],coordinates[self.coord_index][1],0)
+            target_x = target.x_val
+            target_y = target.y_val
+
+            return math.sqrt((pos_x-target_x)**2+(pos_y-target_y)**2)
+
+    
+    def getSpeed(self,car_name):
+        if car_name == "Car1": car_state = self.client.getCarState()
+        if car_name == "Car2": car_state = self.client2.getCarState()
+
+        return car_state.speed
+
+    def autoDrive(self):
         try:
             time.sleep(0.04)
             if self.coordinates_degree_err %90 == 0 and (self.l == 0 and self.r == 0):
@@ -95,12 +196,27 @@ class AutoPilot():
 
             self.car_controller.coord_index = 1
             self.car_controller2.coord_index = 1
+    
+    def drive(self,car_name, action):
+        car_controls = airsim.CarControls()
+        
+        car_controls.steering = action[0]
+        car_controls.throttle = action[1]
+        car_controls.brake = action[2]
+        if car_name == "Car1" :
+            self.client.setCarControls(car_controls)
+            
+
+        if car_name == "Car2" :
+            self.client2.setCarControls(car_controls)
+            
+
 
     def trackCoords(self):
         self.car_controller.keepTrack()
         self.car_controller2.keepTrack()
 
-    def step(self):
+    def step(self,action,car_name):
         self.done = False
 
         # Distance btw cars
@@ -110,7 +226,8 @@ class AutoPilot():
         # Autopilot
         #-----------------#
         if d > 15:
-            self.drive()
+            self.autoDrive()
+            
 
         #-----------------#
 
@@ -119,8 +236,22 @@ class AutoPilot():
         #-----------------#
         else:
             self.keepTrackCoords() # RL 상황 종료 후 지속적인 auto pilot을 위해 사용
-            print("RL Init()")
+            self.drive(car_name,action)
             
+            images = self.getImage(car_name)
+            speed = self.getSpeed(car_name)
+            obs = images.append(speed)
+
+            reward = self.getReward(obs)
+
+            
+
+            if done:
+             client.reset()
+             car_control = interpret_action(1)
+             client.setCarControls(car_control)
+             time.sleep(1)
+             current_step +=1
         
         return None, self.done
 
@@ -139,6 +270,12 @@ if __name__=='__main__':
                 
         print("Episode ", episode_cnt, " Finished")
         episode_cnt += 1
+
+
+        
+
+
+
 
 
         
